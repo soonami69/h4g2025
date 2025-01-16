3aff6b67e3bcec0ac0b9ab212a841da3fbcb40ba
Leong Jia Yi Janna <jannaleong7@gmail.com>
2025-01-17 03:07:37 +0800

    feature : connect meeting form to backend

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,10 @@
-const { createUser, getAllUsers, getUserById, getUserByEmail, updateUser } = require('../controllers/UserController');
+const { createUser, getAllUsers, getUserById, getUserByEmail, updateUser, getNameByEmail } = require('../controllers/UserController');
 const router = require("express").Router();
 
 router.post('/', createUser);
 router.get('/', getAllUsers);
 router.get('/:id', getUserById);
+router.get('/emailName/:email', getNameByEmail)
 router.get('/email/:email', getUserByEmail);
 router.put('/:id', updateUser)
 
