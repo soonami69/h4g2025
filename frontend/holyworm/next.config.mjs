cde2a3b786804f2400ba843d110a50e650d6b9fb
evedaktyl <evedaktyl@gmail.com>
2025-01-17 03:39:09 +0800

    remove key

--- frontend/holyworm/next.config.mjs
+++ frontend/holyworm/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        OPENAI_API_KEY:'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'
     },
 };
 
